ec78f9e13a61952e3e0d9cfb8818e93634e4fde1
Wirehead Software <wireheadsoftware@gmail.com>
2018-10-31 18:37:43 -0700

    Added mission-1-python-basics-takeaways.pdf

---
+++ Python Basics.py
@@ -0,0 +1,57 @@
+# 1. Programming And Data Science
+
+print(1288)
+print(639)
+print(1288 + 639)
+
+# 2. Arithmetic Operators
+
+print((749 + 371 + 828 + 503 + 1379) / 5)
+
+# 3. Variables
+
+anaheim = 371
+print(anaheim)
+albuquerque = 749
+anchorage = 828
+arlington = 503
+atlanta = 1379
+
+# 4. Data Types
+
+atlanta_string = "Atlanta"
+atlanta_float = 1379.5
+
+# 5. The Type Function - type() returns the variable type
+
+atlanta_string = "Atlanta"
+print(type(atlanta_string))
+
+# 6. Using A List To Store Multiple Values
+
+cities = []
+cities.append("Albuquerque")
+cities.append("Anaheim")
+cities.append("Anchorage")
+cities.append("Arlington")
+cities.append("Atlanta")
+
+crime_rates = []
+crime_rates.append(749)
+crime_rates.append(371)
+crime_rates.append(828)
+crime_rates.append(503)
+crime_rates.append(1379)
+
+print(cities)
+print(crime_rates)
+
+# 7. Creating Lists With Values
+
+crime_rates = [749, 371, 828, 503, 1379]
+
+# 8. Comments (# makes comments)
+
+crime_rates = [749, 371, 828, 503, 1379]
+print(crime_rates)
+
